00a38e313e9a64b62056110749101d30d5b52ea6
theblackmoose <83482419+theblackmoose@users.noreply.github.com>
2025-07-11 15:18:54 +0930

    Update portfolio.py

--- services/portfolio.py
+++ services/portfolio.py
@@ -351,7 +351,7 @@ def get_monthly_time_weighted_returns(
     current_app.logger.debug(f"Reindexed TWR:\n{monthly_twr}")
 
     return pd.DataFrame({
-        "month":  monthly_twr.index.strftime("%b-%y"),
+        "month":  monthly_twr.index.strftime("%b %y"),
         "twr_pct": monthly_twr.values
     })
     
